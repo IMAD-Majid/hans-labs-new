f60f2fa680c62f63d6e34c06b6974cfb433fb710
IMAD-Majid <imadvlp0@gmail.com>
2024-02-17 23:55:43 +0000

    icon only changes when you hold the button, not on hover

--- js/script.js
+++ js/script.js
@@ -57,9 +57,24 @@ if (diff <= 0) {
 const contactUsBTN = document.querySelector("#input-box button");
 const tgicon = contactUsBTN.querySelector("img");
 
+// hovering
 contactUsBTN.addEventListener("mouseover", () => {
   tgicon.src = "tgicon_white.png";
+  contactUsBTN.classList.add("hovered")
 })
+// holding finger
+contactUsBTN.addEventListener("touchstart", () => {
+  tgicon.src = "tgicon_white.png";
+  contactUsBTN.classList.add("hovered")
+})
+
+// lifted finger
+contactUsBTN.addEventListener("touchend", () => {
+  tgicon.src = "tgicon_black.png";
+  contactUsBTN.classList.remove("hovered")
+})
+// left button
 contactUsBTN.addEventListener("mouseleave", () => {
   tgicon.src = "tgicon_black.png";
+  contactUsBTN.classList.remove("hovered")
 })
