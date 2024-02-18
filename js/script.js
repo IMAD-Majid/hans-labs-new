54598cd9300cb4ad3eb4d5b739f7facb4314b3f7
IMAD-Majid <imadvlp0@gmail.com>
2024-02-18 00:07:56 +0000

    Fixed displaying timer values after surpassing the deadline

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ const diff = new Date("2024-02-25").getTime() - new Date().getTime();
 if (diff <= 0) {
   seconds.textContent = '00';
   minutes.textContent = '00';
-  hours.textContent   = '00';
-  days.textContent    = '00';
+  hours.textContent = '00';
+  days.textContent = '00';
 } else {
   // setting values of the countdown based on the difference
   let secValue = Math.floor(diff / 1000) % 60,
@@ -19,7 +19,12 @@ if (diff <= 0) {
     hourValue = Math.floor(diff / 1000 / 60 / 60) % 24,
     dayValue = Math.floor(diff / 1000 / 60 / 60 / 24);
 
-const timeFunction = setInterval(() => {
+  const timeFunction = setInterval(() => {
+    seconds.textContent = secValue < 10 ? `0${secValue}` : secValue;
+    minutes.textContent = minValue < 10 ? `0${minValue}` : minValue;
+    hours.textContent = hourValue < 10 ? `0${hourValue}` : hourValue;
+    days.textContent = dayValue < 10 ? `0${dayValue}` : dayValue;
+    
     if (secValue === 0) {
       secValue = 60;
       if (minValue === 0) {
@@ -36,11 +41,6 @@ const timeFunction = setInterval(() => {
       minValue--;
     }
     secValue--;
-  
-    seconds.textContent = secValue < 10 ? `0${secValue}` : secValue;
-    minutes.textContent = minValue < 10 ? `0${minValue}` : minValue;
-    hours.textContent = hourValue < 10 ? `0${hourValue}` : hourValue;
-    days.textContent = dayValue < 10 ? `0${dayValue}` : dayValue;
   }, 1000); //1000ms = 1s
 }
 
