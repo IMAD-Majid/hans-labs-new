4ba24b4fb491ab13892e505a2789b0b7b67f78d0
Han <160033214+HanELT@users.noreply.github.com>
2024-02-19 15:37:26 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,23 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const sideNav = document.getElementById('sideNav');
   const toggleButton = document.getElementById('toggleButton');
   const pageTitle = document.getElementById('pageTitle');
-  const overlay = document.getElementById('overlay');
-
-  // Function to toggle overlay visibility
-  function toggleOverlay() {
-    sideNav.style.width = (sideNav.style.width === '250px') ? '0' : '250px';
-    overlay.style.display = (overlay.style.display === 'block') ? 'none' : 'block';
-  }
+  const overlayToggleButton = document.getElementById('overlayToggleButton');
 
   // Function to update page title and close overlay
   function updatePage(title) {
     pageTitle.textContent = title;
-    toggleOverlay();
+    toggleNavMenu(); // Utilize the new function
   }
 
   // Add event listener for the toggle button
   toggleButton.addEventListener('click', function () {
-    toggleOverlay();
+    toggleNavMenu();
   });
 
   // Add event listeners for navigation links
